45a21d516363a7d93c9948614ddf2e8fab9e4191
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-11-20 09:09:34 -0700

    Add messages to results page

--- frontend/src/pages/ResultsPage.tsx
+++ frontend/src/pages/ResultsPage.tsx
@@ -10,10 +10,19 @@ export default function ResultsPage() {
   const score = state?.score ?? 0;
   const total = state?.total ?? 0;
   const pct = total ? Math.round((score / total) * 100) : 0;
+  let title = "";
+
+  if (score < 5) {
+    title = "novice";
+  } else if (score < 7) {
+    title = "Pro";
+  } else {
+    title = "Trivia Master";
+  }
 
   return (
     <div className="container mt-5 text-center">
-      <h2>Results</h2>
+      <h2>Results: You are a {title}</h2>
       <p className="fs-4 mt-3">
         Congratulations! You got <strong>{score}</strong> out of{" "}
         <strong>{total}</strong> correct ({pct}%).
